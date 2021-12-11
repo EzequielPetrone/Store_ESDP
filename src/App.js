6e16934bf1ee84e9bc094689622666716d8fef46
EzequielPetrone <89227176+EzequielPetrone@users.noreply.github.com>
2021-12-11 09:47:37 -0300

    COMMIT BASE

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+export default function App() {
+  return (
+    <h1 className="text-3xl font-bold underline text-green-500">
+      Hello world!
+    </h1>
+  )
+}
