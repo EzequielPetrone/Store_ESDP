e0c1bdc1e4d729999e149fa96562dc465135584c
EzequielPetrone <89227176+EzequielPetrone@users.noreply.github.com>
2022-01-15 19:39:42 -0300

    Listo entrega Firebase

--- src/index.js
+++ src/index.js
@@ -2,20 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCT7PqXbXA97biXYeHYCeE6wPfLSC4JA7s",
-  authDomain: "store-esdp.firebaseapp.com",
-  projectId: "store-esdp",
-  storageBucket: "store-esdp.appspot.com",
-  messagingSenderId: "345904518545",
-  appId: "1:345904518545:web:d3f7c5b0b637deff3f3630",
-  measurementId: "G-9WMKF9QHEW"
+  apiKey: process.env.REACT_APP_FB_APIKEY,
+  authDomain: process.env.REACT_APP_FB_AUTHDOMAIN,
+  projectId: process.env.REACT_APP_FB_PROJECTID,
+  storageBucket: process.env.REACT_APP_FB_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FB_MESSAGINGSENDERID,
+  appId: process.env.REACT_APP_FB_APPID,
+  measurementId: process.env.REACT_APP_FB_MEASUREMENTID
 };
 
 // Initialize Firebase
